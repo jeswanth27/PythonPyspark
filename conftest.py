1630128f86f29bd80079773bd1fa1103237e3034
Jeswanth <jeswanth27@gmail.com>
2024-02-17 23:43:18 +0530

    Added the pytest fixture for creation of spark session and changed the code in test_retail_proj

---
+++ conftest.py
@@ -0,0 +1,8 @@
+import pytest
+from lib.Utils import get_spark_session 
+@pytest.fixture
+def spark():
+    "createsasparksession"
+    spark_session= get_spark_session("LOCAL")
+    yield spark_session
+    spark_session.stop()
