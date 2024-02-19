9dd94a1128dec463f25b238a9d4931b06389ee28
Jeswanth <jeswanth27@gmail.com>
2024-02-19 20:49:38 +0530

    * Created one more test case for count customer state. * Added new state_aggregate.csv file
    * pipenv run pytest --fixtures - This command is which will print all fixtures with default fixtures

--- conftest.py
+++ conftest.py
@@ -6,3 +6,13 @@ def spark():
     spark_session= get_spark_session("LOCAL")
     yield spark_session
     spark_session.stop()
+
+@pytest.fixture
+def expected_results(spark):
+    "gives the expected results"
+    result_schema="state string,count int"
+    return spark.read \
+        .option("header",True) \
+        .format("csv") \
+        .schema(result_schema) \
+        .load("data/state_aggregate2.csv")
--- test_retail_proj.py
+++ test_retail_proj.py
@@ -2,6 +2,7 @@ import pytest
 
 from lib.Utils import get_spark_session
 from lib.DataReader import read_customers,read_orders
+from lib.DataManipulation import count_orders_state
 
 def test_read_customers(spark):
     count=read_customers(spark,"LOCAL").count()
@@ -9,3 +10,9 @@ def test_read_customers(spark):
 def test_read_orders(spark):
      count_order=read_orders(spark,"LOCAL").count()
      assert count_order==68884
+
+def test_count_orders_state(spark,expected_results):
+    cust_df=read_customers(spark,"LOCAL")
+    result=count_orders_state(cust_df)
+    assert result.collect()==expected_results.collect()
+
